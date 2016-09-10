06c95e2f460b21da501b0bb4eef2c2b6e5f6bc52
Akshay Chawla <chawla.akshay1234@gmail.com>
2016-09-11 00:45:12 +0530

    1. Warping works as expected. 2. Tested the algorithm on test image and logo. 3. The logo pixel has to be multiplied by 255, must allow for logo and    test image to be in the same base. (0-255).
    Build: Passing

--- test_warp.py
+++ test_warp.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimage
 import warp 
-
+import time
 
 def main():
 	'''Test the warp function in module warp.py '''
@@ -23,16 +23,12 @@ def main():
 	print op[0]/op[2]
 	print op[1]/op[2]
 
-	
+	# warping
+	video_copy = warp.inverse_warp(img_logo, img_dest, H, points_image)
 
-
-
-	
-	
-
-	
-
-
+	# show the output
+	plt.imshow(video_copy)
+	plt.show()
 
 if __name__ == '__main__':
 	main()
--- warp.py
+++ warp.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pprint
+import matplotlib.path as mplPath
 
 def estimate_homography(logo_pts, video_pts):
 	'''estimates the H matrix (homography) that maps points from destination to source,
@@ -33,4 +34,40 @@ def estimate_homography(logo_pts, video_pts):
 
 	return H 
 
+def inverse_warp(logo, video, H, video_pts):
+	'''Warp the logo image onto the video image between the video_pts using H (homography) 
+	calculated by the warp.estimate_homography function'''
+
+	# create a copy of the original video image
+	#video_copy = np.zeros(video.shape[0:3])
+	video_copy = np.copy(video)
+	
+	# create set of points that have to be mapped from the logo image
+	bbPath = mplPath.Path(video_pts)
+	valid_loc = []
+	for x in range(video.shape[1]):
+		for y in range(video.shape[0]):
+			if (bbPath.contains_point((x,y))):
+				valid_loc.append([x,y])
+				
+
+	# Iterate through the list
+	for pt in valid_loc:
+		
+		# Projecting points from video to logo 
+		pt_lt = np.dot(H, np.array([[pt[0]], [pt[1]], [1] ]) )
+		x_l = round(pt_lt[0]/pt_lt[2])
+		y_l = round(pt_lt[1]/pt_lt[2])
+		
+		# applying safe limits to projected points
+		x_l = max(x_l,0)
+		x_l = min(x_l,logo.shape[1]-1)
+		y_l = max(y_l,0)
+		y_l = min(y_l,logo.shape[0]-1)
+
+		# copying over pixel data
+		video_copy[pt[1],pt[0]] = logo[y_l, x_l]*255
+
+	# return the video_copy 
+	return video_copy
 
